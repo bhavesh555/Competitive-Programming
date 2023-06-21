1faf807ea2756184e0761920e62202a1c50c3929
Bhavesh Chandani <77831291+bhavesh555@users.noreply.github.com>
2023-06-21 14:00:29 +0530

    java

---
+++ Practice Problems/50A.java
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/50/A
+
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int k=n*m/2;
+        System.out.println(k);
+       
+    }
+    
+}
